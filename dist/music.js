a2e0e9a88e17af3f0752eb012c99ea2149e34835
daisiesloves <daisiesloves.smile@gmail.com>
2022-10-10 17:18:33 +0800

    Site updated: 2022-10-10 17:18:32

---
+++ dist/music.js
@@ -0,0 +1,19 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+	  {
+        name: '时代を超える想い2',
+        artist: '和田熏',
+        url: 'https://www.ytmp3.cn/down/54475.mp3',
+        //cover: 'http://p2.music.126.net/ggnyubDdMxrhpqYvpZbhEQ==/3302932937412681.jpg?param=130y130',
+      },
+      //{
+      //  name: 'Good Time',
+      //  artist: 'Owl City&Carly Rae Jepsen',
+      //  url: 'http://www.ytmp3.cn/down/34148.mp3',
+      //  cover: 'http://p1.music.126.net/c5NVKUIAUcyN4BQUDbGnEg==/109951163221157827.jpg?param=130y130',
+      //}
+    //]
+});
